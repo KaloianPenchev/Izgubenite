04e7e1d2ae9f18c381811910592137181eec9c4e
Vladimir <gerganovvladimir@gmail.com>
2024-03-14 19:51:54 +0200

    fixed issues

--- Website/static/JS/app.js
+++ Website/static/JS/app.js
@@ -2,6 +2,4 @@ var questions = ["How much is the square root of 25?"]
 var pos_choisses = [["2", "5", "4", "7"]];
 var answer = ["5"]
 
-for(i = 65; i<= 68;i++){
-    var cur = getElementById("i")
-}
+
