d6d7b789546a8953c214a25f9562c91f13a6c87f
Vladimir <gerganovvladimir@gmail.com>
2024-03-14 19:30:21 +0200

    fixed issues

--- Website/static/JS/app.js
+++ Website/static/JS/app.js
@@ -1,3 +1,7 @@
-function my(){
+var questions = ["How much is the square root of 25?"]
+var pos_choisses = [["2", "5", "4", "7"]];
+var answer = ["5"]
 
+for(i = 65; i<= 68;i++){
+    var cur = getElementById("i")
 }
