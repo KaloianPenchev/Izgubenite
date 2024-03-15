c4892510eabf0701e89820fa5e049c292f1255f5
Vladimir <gerganovvladimir@gmail.com>
2024-03-15 14:13:10 +0200

    fixed questions algorithm

--- Testing3/app3.js
+++ Testing3/app3.js
@@ -3,12 +3,61 @@ nuCategory.set(0, "Arithmetic");
 nuCategory.set(1, "Geometry");
 nuCategory.set(2, "History");
 nuCategory.set(3, "Geography");
-var questions = [["Корен квадрат от 25", "Корен квадрат от 169", "Корен от 289 е:", "Колко е 1 на степен 0?", "Колко е 3 на степен 4?", "4 to the power of 2"], [""], [""], [""]];
-var pos_choisses = [[["2", "5", "4", "7"], ["14", "13", "9", "28561"], ["17", "13", "21", "14"], ["1", "0", "0.5", "-1"], ["126", "144", "12", "412"], ["12", "8", "16", "32"]],[[]], ,[[]], ,[[]]];
-var answer = [["5", "13", "17", "1", "144", "16"], [], [], []];
+var questions = [
+    ["Корен квадрат от 25", "Корен квадрат от 169", "Корен от 289 е:", "Колко е 1 на степен 0?", "Колко е 3 на степен 4?", "4 на 2 степен е?"],
+    ["Каква е формулата за лице на кръг?", "Колко е лицето на кръг с диаметър 16cm", ],
+    [""],
+    [""]
+];
+var pos_choisses = [
+    [["2", "5", "4", "7"],
+        ["14", "13", "9", "28561"],
+        ["17", "13", "21", "14"],
+        ["1", "0", "0.5", "-1"],
+        ["126", "81", "12", "412"],
+        ["12", "8", "16", "32"]
+    ],
+    [
+        ["π*r*r", "π*π*r", "2*π*r", "4*r*r"],
+        ["16π", "8π", "256π", "72π"],
+    ],
+    [],
+    []
+];
+var answer = [
+    ["5", "13", "17", "1", "81", "16"],
+    ["π*r*r", "16π"],
+    [],
+    []];
 var should_you_do = []
 var quest = 0;
 var category = 0;
+
+function leaveOnlyMain3(){
+    var yum = document.getElementById("main");
+    var xum = document.getElementById("main2");
+    var zum = document.getElementById("main3");
+    xum.style.display = "none";
+    yum.style.display = "none";
+    zum.style.display = "block";
+}
+function isAllCorrect(){
+    for(let i = 0; i < should_you_do[category].length; i++){
+        if(should_you_do[category][i] === true)return false;
+    }
+    return true;
+}
+function change_place(){
+    var yum = document.getElementById("main");
+    var xum = document.getElementById("main2");
+    if (xum.style.display === "none") {
+      xum.style.display = "block";
+      yum.style.display = "none";
+    } else {
+      xum.style.display = "none";
+      yum.style.display = "block";
+    }
+}
 function printQuestion(){
     document.getElementById("question").innerHTML = questions[category][quest];
 }
@@ -29,6 +78,7 @@ function checkIfCorrect() {
     }
     if (ans) {
         should_you_do[category][quest] = false;
+        console.log(should_you_do[category][quest]);
         console.log("Correct");
     } else {
         console.log("Incorrect");
@@ -40,7 +90,16 @@ function checkIfCorrect() {
 
 
 function whenStart(){
+    if(quest === 0){
+        if(isAllCorrect()){
+            leaveOnlyMain3();
+            return;
+        }
+        change_place();
+    }
     if(quest >= questions[category].length){
+        quest = 0;
+        change_place();
         return;
     }
     if(should_you_do[category][quest] === false){
@@ -50,7 +109,6 @@ function whenStart(){
     }
     printQuestion();
     printChoisses();
-
 }
 
 function Release(s) {
--- Website/static/JS/questions.js
+++ Website/static/JS/questions.js
@@ -36,6 +36,7 @@ var questions = [
         
     ]
 ];
+
 var pos_choisses = [
     [
 	    ["2", "5", "4", "7"], 
@@ -78,9 +79,40 @@ var answer = [
     ["Индонезия", "Русия", "Непал", "Канбера", "71%"],
 
 ];
+const nuCategory = new Map();
+nuCategory.set(0, "Arithmetic");
+nuCategory.set(1, "Geometry");
+nuCategory.set(2, "History");
+nuCategory.set(3, "Geography");
 var should_you_do = []
 var quest = 0;
 var category = 0;
+
+function leaveOnlyMain3(){
+    var yum = document.getElementById("main");
+    var xum = document.getElementById("main2");
+    var zum = document.getElementById("main3");
+    xum.style.display = "none";
+    yum.style.display = "none";
+    zum.style.display = "block";
+}
+function isAllCorrect(){
+    for(let i = 0; i < should_you_do[category].length; i++){
+        if(should_you_do[category][i] === true)return false;
+    }
+    return true;
+}
+function change_place(){
+    var yum = document.getElementById("main");
+    var xum = document.getElementById("main2");
+    if (xum.style.display === "none") {
+      xum.style.display = "block";
+      yum.style.display = "none";
+    } else {
+      xum.style.display = "none";
+      yum.style.display = "block";
+    }
+}
 function printQuestion(){
     document.getElementById("question").innerHTML = questions[category][quest];
 }
@@ -101,6 +133,7 @@ function checkIfCorrect() {
     }
     if (ans) {
         should_you_do[category][quest] = false;
+        console.log(should_you_do[category][quest]);
         console.log("Correct");
     } else {
         console.log("Incorrect");
@@ -112,7 +145,16 @@ function checkIfCorrect() {
 
 
 function whenStart(){
+    if(quest === 0){
+        if(isAllCorrect()){
+            leaveOnlyMain3();
+            return;
+        }
+        change_place();
+    }
     if(quest >= questions[category].length){
+        quest = 0;
+        change_place();
         return;
     }
     if(should_you_do[category][quest] === false){
@@ -122,7 +164,6 @@ function whenStart(){
     }
     printQuestion();
     printChoisses();
-
 }
 
 function Release(s) {
