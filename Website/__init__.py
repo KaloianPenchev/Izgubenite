554d970d1689bdcf57494bebc06ea654ed19a4e5
MRT-09 <martin.v.marinov.2023@elsys-bg.org>
2024-03-15 09:49:25 +0200

    added static path

--- Website/__init__.py
+++ Website/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/static')
     app.config['SECRET_KEY'] = 'ao487gfap;fhupafbas;gaef.c.faeae.kfcad1uy12r3'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
